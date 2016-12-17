2157911fb3c26e70b6f6e88e7abac65f4dd7af99
kawakubox <shigefumi.kawakubo@gmail.com>
2016-12-17 20:54:46 +0900

    Create rbenv recipe

---
+++ cookbooks/rbenv/default.rb
@@ -0,0 +1,20 @@
+require 'pry'
+include_recipe 'rbenv::system'
+
+file '/home/ec2-user/.bashrc' do
+  action :create
+end
+
+file '/home/ec2-user/.bashrc' do
+  action :edit
+  block do |content|
+    next if content.match %r{export RBENV_ROOT=/usr/local/rbenv}
+    appendix = <<~EOS
+      # rbenv
+      export RBENV_ROOT=/usr/local/rbenv
+      export PATH="${RBENV_ROOT}/bin:${PATH}"
+      eval "$(rbenv init -)"
+    EOS
+    content << appendix
+  end
+end
